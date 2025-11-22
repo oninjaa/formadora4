6db181f92ad9d18f1cb636657515b48347d288c8
Lucas Leonardo <xmiiitz@gmail.com>
2025-11-22 19:47:44 -0300

    fix: gradle

--- android/app/src/main/java/com/jogosquiz/formadoraiv/MainActivity.java
+++ android/app/src/main/java/com/jogosquiz/formadoraiv/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.jogosquiz.formadoraiv;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'formadoraIV',
+  appId: 'com.jogosquiz.formadoraiv',
+  appName: 'FormadoraIVQuiz',
   webDir: 'www'
 };
 
